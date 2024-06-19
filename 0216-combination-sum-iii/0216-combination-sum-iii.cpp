c63e88555536fe38fcc4b3296f07a1af0614c11d
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2024-06-19 14:51:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,27 +1,31 @@
 class Solution {
-
 public:
-    void help(int ind, int sum, int k, vector<vector<int>>& ans, vector<int>& v, int a[]) {
-            if (sum == 0 && v.size() == k)
-            {
-                ans.push_back(v);
-            return;
+    vector<vector<int>> re;
+    void func(vector<int>& ls,vector<int>& ds,int i,int k,int n)
+    {
+        if(i>=9)
+        return;
+        if(ds.size()==k)
+        {
+            if(n==0)
+            re.push_back(ds);
+            return ;
         }
-        else if(ind>=9)
-        return ;
-        if (a[ind] <= sum) {
-            v.push_back(a[ind]);
-            help(ind + 1, sum - a[ind], k, ans, v, a);
-            v.pop_back();  
+        if(ls[i]<=n)
+        {
+            ds.push_back(ls[i]);
+            func(ls,ds,i+1,k,n-ls[i]);
+            ds.pop_back();
+            func(ls,ds,i+1,k,n);
         }
-        help(ind + 1, sum, k, ans, v, a);
+        
     }
 
-    vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
-        vector<int> v;
-        int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        help(0, n, k, ans, v, a);
-        return ans;
+    vector<vector<int>> combinationSum3(int k, int n) 
+    {
+        vector<int> ls={1,2,3,4,5,6,7,8,9};
+        vector<int> ds;
+        func(ls,ds,0,k,n);
+        return re;
     }
 };
