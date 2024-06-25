c9ea12d854cd0756999ba24f11fc04978982d02f
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2024-06-25 15:56:13 +0530

    Time: 314 ms (21.03%), Space: 16.1 MB (75.49%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -12,12 +12,41 @@ public:
         return dp[i][s]=p|np;
     }
 
+    bool isSubsetSum(vector<int>arr, int sum)
+    {
+        // code here 
+        //vector<vector<int>>dp(arr.size(),vector<int>(sum+1,-1));
+        vector<vector<bool>>dp(arr.size(),vector<bool>(sum+1,0));
+        for(int i=0;i<arr.size();i++)
+        dp[i][0]=true;
+        //dp[0][arr[0]]=true;
+        
+        for(int i=1;i<arr.size();i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                bool nt =dp[i-1][j];
+                bool t=false;
+                if(arr[i]<=j)
+                t=dp[i-1][j-arr[i]];
+                dp[i][j]=t|nt;
+            }
+        }
+        
+        return dp[arr.size()-1][sum];
+    }
+
     bool canPartition(vector<int>& nums) 
     {
         int k=0;
         for(int i=0;i<nums.size();i++)
         k+=nums[i];
-        vector<vector<int>> dp(nums.size(),vector<int>(k,-1));
-        return func(nums,nums.size(),0,0,k,dp);    
+
+        // vector<vector<int>> dp(nums.size(),vector<int>(k,-1));
+        // return func(nums,nums.size(),0,0,k,dp);
+
+        if(k%2) return false;
+
+        return isSubsetSum(nums,k/2);
     }
 };
