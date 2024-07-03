8c7232b1d8bfa0bc54a90e98dc79f8ffaed76342
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2024-07-03 13:18:58 +0530

    Time: 27 ms (49.9%), Space: 25.7 MB (54.29%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -19,7 +19,22 @@ public:
     {
         int i=s.size();
         int j=t.size(); 
-        vector<vector<int>> dp(i,vector<int>(j,-1));
-        return f(i-1,j-1,s,t,dp);
+        vector<vector<int>> dp(i+1,vector<int>(j+1,0));
+        // for(int a=0;a<=i;a++)
+        // dp[a][0]=0;
+        // for(int a=0;a<=j;a++)
+        // dp[0][a]=0;
+        for(int a=1;a<=i;a++)
+        {
+            for(int b=1;b<=j;b++)
+            {
+                if(s[a-1]==t[b-1])
+                dp[a][b]=1+dp[a-1][b-1];
+                else
+                dp[a][b]=max(dp[a-1][b],dp[a][b-1]);
+            }
+        }
+        return dp[i][j];
+        //return f(i-1,j-1,s,t,dp);
     }
 };
