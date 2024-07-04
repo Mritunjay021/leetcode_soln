b782c2982fb096dbe89466ea325c709ec7c1f8cf
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2024-07-04 21:49:12 +0530

    Time: 3 ms (34.84%), Space: 12.2 MB (5.88%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int help(vector<int>& nums, int s,int i,int p,int sum) 
+    int help(vector<int>& nums, int s,int i,int p,int sum,vector<vector<int>>& dp) 
     {
         if(s==0)
         return p;
@@ -8,24 +8,28 @@ public:
         {
             return 0;
         }
+        if(dp[i][s]!=-1)
+        return dp[i][s];
+        
+        int nt = help(nums, s,i + 1, p,sum,dp);
         int t = 0;
         if (s >= nums[i])
-            t = help(nums,s - nums[i],i, p * nums[i],sum);
+            t = help(nums,s - nums[i],i, p * nums[i],sum,dp);
         
-        int nt = help(nums, s,i + 1, p,sum);
         
-        return max(t, nt);
+        return dp[i][s]=max(t, nt);
     }
     
-    int integerBreak(int s) {
+    int integerBreak(int s) 
+    {
         vector<int> nums;
-        //int s=1;
+        int su=1;
         for (int i = 1; i < s; i++) 
         { 
             nums.push_back(i);
+            su+=i;
         }
-        int p=1;
-        //vector<int> dp(s,-1);
-        return help(nums,s,0,p,s);
+        vector<vector<int>> dp(nums.size()+1,vector<int> (su+1,-1));
+        return help(nums,s,0,1,s,dp);
     }
 };
