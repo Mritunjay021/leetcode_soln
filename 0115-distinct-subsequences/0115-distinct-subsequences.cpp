9078e5f2d0b558312d05e9fc04d0d7365cda41a8
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2024-07-04 23:55:28 +0530

    Time: 16 ms (69.04%), Space: 31.4 MB (24.73%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -20,8 +20,24 @@ public:
     {
         int l1=s.size();
         int l2=t.size();
-        vector<vector<int>> dp(l1,vector<int>(l2,-1));
-        return f(s,t,l1-1,l2-1,dp);
-        //for(int i=0;i<l1;i++)
+        vector<vector<double>> dp(l1+1,vector<double>(l2+1,0));
+        // return f(s,t,l1-1,l2-1,dp);
+        for(int i=0;i<=l1;i++)
+        dp[i][0]=1;
+
+        for(int i=1;i<=l2;i++)
+        dp[0][i]=0;
+        
+        for(int i=1;i<=l1;i++)
+        {
+            for(int j=1;j<=l2;j++)
+            {
+                if(s[i-1]==t[j-1])
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+        return (int)dp[l1][l2];
     }
 };
