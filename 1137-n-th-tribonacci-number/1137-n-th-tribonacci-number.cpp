2f41fac8d21c5dcdaf201a40e8cfd87c53069a12
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2024-09-28 11:30:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    int func(int n,int a,int b,int c,int i)
+    {
+        if(i==n-1)
+        return c;
+        return func(n,b,c,a+b+c,i+1);
+        
+    }
+
+
+    int tribonacci(int n) 
+    {
+        int s=0; 
+        if(n==0)
+        return 0;
+        return func(n,0,1,1,1); 
+    }
+};
