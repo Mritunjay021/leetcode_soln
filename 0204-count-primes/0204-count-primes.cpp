37077006574423d235b3a4a6aa757076a7fa31c6
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-01-16 01:34:48 +0530

    Time: 188 ms (63.12%), Space: 12.6 MB (59.77%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -8,7 +8,7 @@ public:
 
         for (int i = 2; i * i < n; i++) {
             if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
+                for (int j = i * 2; j < n; j += i) {
                     isPrime[j] = false;
                 }
             }
