a7ed8f2603e8d2227770a063d99fa694b044312b
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-01-18 19:39:58 +0530

    Time: 0 ms (100%), Space: 13.7 MB (35.02%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -13,15 +13,11 @@ public:
             int mid=(l+r)/2;
             if(target==nums[mid])
             return mid;
-            else if(nums[mid]<target && nums[mid+1]>target) return mid+1;
-
-            else if(nums[mid]>target && nums[mid-1]<target) return mid;
-
             else if(target < nums[mid])
             r=mid-1;
-
-            else l=mid+1;
+            else 
+            l=mid+1;
         }    
-        return -1;
+        return l;    
     }
 };
