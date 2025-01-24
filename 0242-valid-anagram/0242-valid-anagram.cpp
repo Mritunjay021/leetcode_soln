a5557c35c0d706f6c0c85bcb44e4444719852ef9
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-01-24 20:57:22 +0530

    Time: 0 ms (100%), Space: 9.6 MB (73.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
+        
         if(s.length()!=t.length())
         return false;
         if(s==t)
         return true;
-        int a1[26]={0},a2[26]={0};
+        int a[26]={0};
         for(int i=0;i<s.length();i++)
         {
-            a1[s[i]-97]+=1;
-            a2[t[i]-97]+=1;
+            a[s[i]-97]+=1;
+            a[t[i]-97]-=1;
         }
         for(int i=0;i<26;i++)
         {
-            if(a1[i]!=a2[i])
+            if(a[i]!=0)
             return false;
         }
-        return true;
+        return true;    
     }
 };
