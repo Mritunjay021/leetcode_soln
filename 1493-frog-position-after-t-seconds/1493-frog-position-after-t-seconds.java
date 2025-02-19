ebba0e5003c4c71162f4059b085de4f76bc472dc
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-02-19 19:31:05 +0530

    Time: 2 ms (100%), Space: 44.8 MB (67.38%) - LeetHub

---
+++ 1493-frog-position-after-t-seconds/1493-frog-position-after-t-seconds.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<Integer>[] adj;
+    int time;
+
+    public double frogPosition(int n, int[][] edges, int t, int target) {
+        time = t;
+        adj = new List[n + 1];
+        for (int i = 0; i <= n; i++)
+            adj[i] = new ArrayList<>();
+        for (int i = 0; i < edges.length; i++) {
+            adj[edges[i][0]].add(edges[i][1]);
+            adj[edges[i][1]].add(edges[i][0]);
+        }
+        adj[1].add(-1);
+        return probability(1, target, 0, -1);
+    }
+
+    private double probability(int node, int target, int t, int parent) {
+        List<Integer> adjlist = adj[node];
+        int childCount = adjlist.size() - 1;
+        if (t == time || childCount == 0)
+            return node == target ? 1.0 : 0.0;
+        double prob = 0.0;
+        for (int child : adjlist) {
+            if (child == parent)
+                continue;
+            prob += probability(child, target, t + 1, node);
+        }
+        return prob / childCount;
+    }
+}
