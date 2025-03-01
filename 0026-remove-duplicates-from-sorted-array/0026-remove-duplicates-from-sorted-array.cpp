23acc09977f829a1a94788b12403d066fb78643a
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-03-01 20:12:50 +0530

    Time: 0 ms (100%), Space: 22.8 MB (19.64%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
-    int search(vector<int> nums, int beg) {
-        if (beg == nums.size() - 1 || nums[beg] < nums[beg + 1])
-            return beg + 1;
+    int removeDuplicates(vector<int>& nums) 
+    {
+        int slow=0,fast=1;
 
-        int i = beg, j = nums.size() - 1;
-        while (i <= j) {
-            int mid = (i + j) / 2;
-            if (nums[mid] > nums[i] && nums[mid - 1] == nums[i])
-                return mid;
-            else if (nums[mid] == nums[i] &&
-                     (mid + 1 == nums.size() || nums[mid + 1] > nums[i]))
-                return mid + 1;
-            else if (nums[mid] > nums[i])
-                j = mid - 1;
-            else
-                i = mid + 1;
-        }
-        return 0;
-    }
-
-    int removeDuplicates(vector<int>& nums) {
-        int l = nums.size();
-        int n = 0, c = 0;
-        for (int i = 0; i < l; i = n) {
-            n = search(nums, i);
-            nums[c] = nums[i];
-            c++;
-        }
-        return c;
+        while(fast<nums.size())
+        {
+            if(nums[slow]!=nums[fast])
+            {
+                slow++;
+                nums[slow]=nums[fast];
+            }
+            fast++;
+        }    
+        return slow+1;
     }
 };
