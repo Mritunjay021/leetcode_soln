d0fd4f7d37dbcf4ddfef37d337d4272fee4f3950
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-03-07 00:44:18 +0530

    Time: 6 ms (46.49%), Space: 24.7 MB (38.92%) - LeetHub

--- 1277-largest-multiple-of-three/1277-largest-multiple-of-three.cpp
+++ 1277-largest-multiple-of-three/1277-largest-multiple-of-three.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+
+    int func(vector<int>& count,int s)
+    {
+        while(s<10)
+        {
+            if(count[s]>0)
+            {
+                count[s]--;
+                return 1;
+            }
+            s+=3;
+        }
+        return 0;
+    }
+
     string largestMultipleOfThree(vector<int>& digits) 
     {
         if(digits.size()==1)
@@ -9,7 +24,7 @@ public:
             else
             return to_string(digits[0]);
         }
-        sort(digits.begin(), digits.end(), greater<int>());  // Sort in descending order
+        sort(digits.begin(), digits.end(), greater<int>());
         
         vector<int> count(10, 0);
         int sum = 0;
@@ -19,26 +34,16 @@ public:
             sum += digit;
         }
 
-        auto removeSmallest = [&](int rem) {
-            for (int i = 0; i <= 9; i++) {
-                if (count[i] > 0 && i % 3 == rem) {
-                    count[i]--;
-                    return true;
-                }
-            }
-            return false;
-        };
-
         if (sum % 3 == 1) {
-            if (!removeSmallest(1)) {
-                removeSmallest(2);
-                removeSmallest(2);
+            if (!func(count, 1)) {
+                // If unable to remove one remainder-1 number, remove two remainder-2 numbers
+                if (!func(count, 2) || !func(count, 2)) return "";
             }
         } 
         else if (sum % 3 == 2) {
-            if (!removeSmallest(2)) {
-                removeSmallest(1);
-                removeSmallest(1);
+            if (!func(count, 2)) {
+                // If unable to remove one remainder-2 number, remove two remainder-1 numbers
+                if (!func(count, 1) || !func(count, 1)) return "";
             }
         }
 
