f1c270f23eb8e6d4fe35d275b6b4293b180b8d84
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-03-11 11:43:15 +0530

    Time: 0 ms (100%), Space: 7.8 MB (84.22%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,13 +2,6 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) 
     {
-        if(n==1)
-        return true;
-        else if(n==0)
-        return false;
-        else if(log2(n)==static_cast<int>(log2(n)))
-        return true;
-        else
-        return false;    
+        return n>0 && (n&(n-1))==0;    
     }
 };
