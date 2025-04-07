1646931884c5e4f20023b8095faede4c04e50ae0
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-04-07 13:20:59 +0530

    Time: 0 ms (100%), Space: 9.6 MB (81.38%) - LeetHub

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) 
+    {
+        vector<int> arr(n);
+        for(int i=0;i<n/2;i++)
+        {
+            arr[i]=i+1;
+            arr[n-i-1]=-(i+1);
+        }    
+        if(!n%2)
+        arr[n/2-1]=0;
+
+        return arr;
+    }
+};
