62d84d3a408cee3c34d407979e0cb1ff6e37527c
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-06-17 00:55:14 +0530

    Time: 20 ms (55.53%), Space: 38.8 MB (44.81%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,45 +1,50 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) 
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
     {
-        int n=grid.size();
-        int m =grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<vector<int>> ans(n,vector<int>(m));    
+        int m=mat.size();
+        int n=mat[0].size();
+
         queue<pair<pair<int,int>,int>> q;
-        for(int i=0;i<n;i++)
+
+        vector<vector<int>> vis(m,vector<int>(n,0));
+
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
-                if(grid[i][j]==0)
+                if(mat[i][j]==0)
                 {
-                    vis[i][j]=1;
                     q.push({{i,j},0});
+                    vis[i][j]=1;
                 }
-                else 
-                vis[i][j]=0;
             }
-        }
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
+        }  
+
+        int dr[4]={-1,0,1,0};
+        int dc[4]={0,-1,0,1};
+
         while(!q.empty())
         {
             int r=q.front().first.first;
             int c=q.front().first.second;
-            int step=q.front().second;
+            int d=q.front().second;
+
             q.pop();
-            ans[r][c]=step;
+
             for(int i=0;i<4;i++)
             {
                 int nr=r+dr[i];
                 int nc=c+dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0)
+                if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc])
                 {
                     vis[nr][nc]=1;
-                    q.push({{nr,nc},step+1});
+                    q.push({{nr,nc},d+1});
+                    mat[nr][nc]=d+1;
                 }
             }
-        }
-        return ans;
+        }  
+
+        return mat;
     }
 };
