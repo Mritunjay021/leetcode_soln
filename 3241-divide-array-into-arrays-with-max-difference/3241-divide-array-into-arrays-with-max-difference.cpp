7db066e6d4b3747d20b683e40c9777f55a022aa4
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-06-18 09:37:26 +0530

    Time: 48 ms (60.53%), Space: 120.2 MB (44.5%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
@@ -5,26 +5,21 @@ public:
         int n=nums.size();
 
         sort(nums.begin(),nums.end()); 
-        vector<int> v;
         vector<vector<int>> vec;  
 
-        for(int i=0;i<n/3;i++)
+        for(int i=0;i<n-2;i+=3)
         {
-            for(int j=0;j<3;j++)
-            {
-                v.push_back(nums[i*3+j]);
-            }
-            vec.push_back(v);
-            v.clear();
+            if(nums[i+2]-nums[i]>k)
+            return {};
+
+            vec.push_back({nums[i],nums[i+1],nums[i+2]});
         }    
 
-        vector<vector<int>> ep;        
-
-        for(int i=0;i<n/3;i++)
-        {
-            if(vec[i][2]-vec[i][0]>k)
-            return ep;
-        }
+        // for(int i=0;i<n/3;i++)
+        // {
+        //     if(vec[i][2]-vec[i][0]>k)
+        //     return {};
+        // }
         return vec;
     }
 };
