ee45cc81791065caa67e8396c7a6aab8ade28e4e
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-06-20 15:35:31 +0530

    Time: 4 ms (68.74%), Space: 19.3 MB (75.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-    bool canFinish(int V, vector<vector<int>>& prerequisites) 
-    {
-        vector<int> adj[V];
-        for(auto it:prerequisites)
-        adj[it[0]].push_back(it[1]);
 
-        vector<int> ed(V,0);
-	   for(int i=0;i<V;i++)
-	   {
-	       for(auto it:adj[i])
-	        ed[it]++;
-	   }
-	   queue<int> q;
-	   for(int i=0;i<V;i++)
-	   {
-	       if(ed[i]==0)
-	       q.push(i);
-	   }
-	   vector<int> ans;
-	   while(!q.empty())
-	   {
-	       int node=q.front();
-	       q.pop();
-	       ans.push_back(node);
-	       for(auto it:adj[node])
-	       {
-	           ed[it]--;
-	           if(ed[it]==0)
-	           q.push(it);
-	       }
-	   }
-        if(ans.size()==V)
+    bool dfs(vector<int>& vis,int i,vector<vector<int>>& adj,vector<int>& pvis)
+    {
+        vis[i]=1;
+        pvis[i]=1;
+
+        for(auto it:adj[i])
+        {
+            if(!vis[it])
+            {
+                if(!dfs(vis,it,adj,pvis))
+                return false;
+            }
+            else if(pvis[it])
+            return false;
+        }
+        pvis[i]=0;
         return true;
-        else
-        return false;
+    }
+
+    bool canFinish(int n, vector<vector<int>>& pre) 
+    {
+        vector<vector<int>> adj(n);
+        vector<int> pvis(n,0),vis(n,0);
+
+        for(int i=0;i<pre.size();i++)
+        adj[pre[i][1]].push_back(pre[i][0]);
+
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+                {
+                    if(!dfs(vis,i,adj,pvis))
+                    return false;
+                }
+        }
+        return true;
     }
 };
