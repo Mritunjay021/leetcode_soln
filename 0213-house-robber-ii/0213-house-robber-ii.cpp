73884371a4594cbc83426e57f02318f9d5439232
Mritunjay Rai <121248684+Mritunjay021@users.noreply.github.com>
2025-07-15 09:48:04 +0530

    Time: 0 ms (100%), Space: 10.9 MB (20.72%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-    int func(vector<int>&  nums,int n,vector<int>& dp)
+    int func(vector<int>& nums,int i,vector<int>& dp)
     {
-        if(n<0)
+        if(i>=nums.size())
         return 0;
-        else if(n==0)
-        return nums[0];
-        else if(dp[n]!=-1)
-        return dp[n];
-        int p=nums[n]+func(nums,n-2,dp);
-        int np=func(nums,n-1,dp);
-        return dp[n]=max(p,np);
+
+        if(dp[i]!=-1)
+        return dp[i];
+
+        int p=nums[i]+func(nums,i+2,dp);
+        int np=func(nums,i+1,dp);
+
+        return dp[i]=max(p,np);
     }
 
     int rob(vector<int>& nums) 
     {
-        if(n==0)
-        return nums[0];
         int n=nums.size();
-        vector<int> dp(n+1,-1);
-        vector<int> dp1(n+1,-1);
-        int a1= func(nums,n-2,dp);
-        nums.erase(nums.begin());
-        int a2=func(nums,n-2,dp1);
-        return max(a1,a2);    
+        if(n==1)
+        return nums[0];
+        
+        vector<int> dp(n,-1);
+
+        int x=func(nums,1,dp);
+
+        nums.erase(nums.end());
+        vector<int> dp1(n-1,-1);
+
+        int y=func(nums,0,dp1);
+
+        return max(x,y);
     }
 };
